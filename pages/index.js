926ffda56318622a21b57237d58f1eaa214aa46a
z0050qg <erik.nelson@target.com>
2021-11-14 07:10:22 -0600

    animation working

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
+import { parseRelativeUrl } from "next/dist/shared/lib/router/utils/parse-relative-url";
 import Head from "next/head";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import HTMLFlipBook from "react-pageflip";
 
 const PageCover = forwardRef((props, ref) => {
@@ -27,7 +28,7 @@ const Page = forwardRef((props, ref) => {
 function MyBook(props) {
   return (
     <HTMLFlipBook
-      width={350}
+      width={550}
       height={733}
       size="stretch"
       minWidth={315}
@@ -38,6 +39,12 @@ function MyBook(props) {
       showCover={true}
       mobileScrollSupport={true}
       className="flip-book"
+      onFlip={(page, direction, stuff) => {
+        console.log("page", page, stuff);
+      }}
+      onChangeState={state => {
+        console.log("state", state);
+      }}
     >
       <PageCover>Lorem ipsum...</PageCover>
 
@@ -52,34 +59,45 @@ function MyBook(props) {
   );
 }
 export default function Home() {
+  const [isYearBookOpen, setIsYearBookOpen] = useState(false);
+  const [isYearBookDisplayed, setIsYearBookDisplayed] = useState(false);
+
+  const onClickYearBook = () => {
+    setIsYearBookOpen(!isYearBookOpen);
+    if (!isYearBookOpen) {
+      setTimeout(() => setIsYearBookDisplayed(true), 1000);
+    } else {
+      setIsYearBookDisplayed(false);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div
+      style={{
+        backgroundImage: 'url("/degenacademy.jpeg")',
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+      className="flex flex-col items-center justify-center min-h-screen py-2"
+    >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <MyBook />
-        </div>
+        <div
+          className={isYearBookOpen ? "openYearbook" : "unopenedYearbook"}
+          onClick={onClickYearBook}
+        ></div>
+        {isYearBookDisplayed ? (
+          <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+            <MyBook />
+          </div>
+        ) : null}
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24 ">
         <a
           className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
