56dda2cc8e8bdf08af8f9e512e22757ac98d815a
z0050qg <erik.nelson@target.com>
2021-12-06 10:14:50 -0600

    pick it back up

---
+++ Components/TableOfContents.js
@@ -0,0 +1,37 @@
+import { useState, useContext } from "react";
+import { PageContext } from "./Book";
+
+const Section = ({ page, children }) => {
+  const { test, switchPage, getCurrentPage } = useContext(PageContext);
+  return (
+    <div className="flex items-center justify-between w-full flex-1 px-5 text-center">
+      <div className="">{children}</div>
+      <button type="button" onClick={() => switchPage(page)}>
+        {page}
+      </button>
+    </div>
+  );
+};
+
+export const TableOfContents = () => {
+  const [isYearBookOpen, setIsYearBookOpen] = useState(false);
+
+  return (
+    <div>
+      <Section page={2}>Directory</Section>
+      <Section page={3}>Principal</Section>
+      <Section page={4}>Faculty</Section>
+      <Section page={5}>Student Council</Section>
+      <Section page={6}>Student Portraits</Section>
+      <Section page={7}>Student Groups</Section>
+      {/* <SubSection page={7}>Mint Squad</SubSection>
+      <SubSection page={8}>Degen Ape Mafia</SubSection>
+      <SubSection page={9}>Degen Divas</SubSection>
+      <SubSection page={10}>Endangered Apes</SubSection>
+      <SubSection page={11}>DAA HK</SubSection>
+      <SubSection page={12}>NB-Apes</SubSection>
+      <SubSection page={9}>Coinaholics</SubSection> */}
+      <Section page={8}>Student Groups</Section>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,7 @@
-import { parseRelativeUrl } from "next/dist/shared/lib/router/utils/parse-relative-url";
 import Head from "next/head";
-import { forwardRef, useState } from "react";
-import HTMLFlipBook from "react-pageflip";
+import { useState } from "react";
+import { Book } from "../Components/Book";
 
-const PageCover = forwardRef((props, ref) => {
-  return (
-    <div className="page page-cover" ref={ref} data-density="hard">
-      <div
-        className="page-content"
-        style={{
-          backgroundImage: 'url("/yearbookcoverbrown.jpg")',
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
-      >
-        <h2>{props.children}</h2>
-      </div>
-    </div>
-  );
-});
-const Page = forwardRef((props, ref) => {
-  return (
-    <div className="page" ref={ref}>
-      <div className="page-content">
-        <h2 className="page-header">Page header - {props.number}</h2>
-        <div className="page-image"></div>
-        <div className="page-text">{props.children}</div>
-        <div className="page-footer">{props.number + 1}</div>
-      </div>
-    </div>
-  );
-});
-
-function MyBook(props) {
-  return (
-    <HTMLFlipBook
-      width={550}
-      height={733}
-      size="stretch"
-      minWidth={315}
-      maxWidth={1000}
-      minHeight={400}
-      maxHeight={1533}
-      maxShadowOpacity={0.5}
-      showCover={true}
-      mobileScrollSupport={true}
-      className="flip-book"
-      onFlip={(page, direction, stuff) => {
-        console.log("page", page, stuff);
-      }}
-      onChangeState={state => {
-        console.log("state", state);
-      }}
-    >
-      <PageCover> </PageCover>
-      <Page number={1}>Table of Contents</Page>
-
-      <Page number={1}>School Administrators</Page>
-      <Page number={2}>Faculty</Page>
-      <Page number={3}>Student Portraits</Page>
-      <Page number={4}>School Events/year in review</Page>
-      <Page number={5}>Student Groups</Page>
-      <Page number={6}>Lorem ipsum...</Page>
-      <PageCover>Lorem ipsum...</PageCover>
-    </HTMLFlipBook>
-  );
-}
 export default function Home() {
   const [isYearBookOpen, setIsYearBookOpen] = useState(false);
   const [isYearBookDisplayed, setIsYearBookDisplayed] = useState(false);
@@ -101,25 +36,7 @@ export default function Home() {
             height: 600,
             width: 1200,
           }}
-        >
-          {/* <div id="leaves" style={{ zIndex: 2 }}>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-            <i></i>
-          </div> */}
-        </div>
+        ></div>
 
         {!isYearBookDisplayed ? (
           <div
@@ -135,11 +52,11 @@ export default function Home() {
         ) : null}
         {isYearBookDisplayed ? (
           <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-            <MyBook />
+            <Book />
           </div>
         ) : null}
       </main>
-
+      {/* 
       <footer className="flex items-center justify-center w-full h-24 ">
         <a
           className="flex items-center justify-center"
@@ -150,7 +67,7 @@ export default function Home() {
           Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
